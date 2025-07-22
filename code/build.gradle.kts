be6896b4a4bbd6a244dd5f006ef33f94c3cef9a3
Prashanth Rudrabhat <prudrabhat@adobe.com>
2025-07-22 14:32:03 -0700

    Migrate to CentralSonatype publishing
    OSS Sonatype publishing is being deprecated in favor of Central Sonatype.
    `aepsdk-commons` was updated with the latest changes required for this changes.
    This PR has necessary changes to adopt the migration changes.
    
    Workflows:
     - Update workflows to use gha-android-3.4.2
     - Set the staging-dir to match the build artifact directories for individual extensions
     - Utilize branch ref option to direct snapshots branches
    
    Gradle:
     - Update to use AEP SDK Gradle Plugin 3.4.0
     - Fetch from central sonatype snapshots instead of OSS.
    
    Makefile:
     - Switch the ci-publish-main and ci-publish-snapshot to trigger the publish command instead.

--- code/build.gradle.kts
+++ code/build.gradle.kts
@@ -7,6 +7,6 @@ buildscript {
         mavenLocal()
     }
     dependencies {
-        classpath("com.github.adobe:aepsdk-commons:gp-3.0.0")
+        classpath("com.github.adobe:aepsdk-commons:gp-3.4.0")
     }
 }
